527dfc306e81788fb3042e45a8bd3031ffe2e2be
SpyderXN <demondrive779@gmail.com>
2023-09-12 12:22:42 +0530

    done

---
+++ lib/models/catalog.dart
@@ -0,0 +1,20 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+}
+
+final product = [
+  Item(
+      "Codepur001",
+      "Apple iphone 12",
+      "Apple iphone 12th generation",
+      999,
+      "#33505a",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Elon_Musk_Colorado_2022_%28cropped2%29.jpg/800px-Elon_Musk_Colorado_2022_%28cropped2%29.jpg")
+];
