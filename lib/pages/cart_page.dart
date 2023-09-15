2adc91905e61fd39005db812f13ac031e95b0d2e
SpyderXN <demondrive779@gmail.com>
2023-09-15 20:43:52 +0530

    done

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -8,7 +8,7 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: MyTheme.creamColor,
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
